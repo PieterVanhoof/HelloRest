f15ca8b4c23684f244657c78af292f79deb63e84
mobapp12 <mobapp12@dt-c205-20.local>
2019-01-29 09:48:57 +0100

    Initial Commit

---
+++ HelloRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloRest
+//
+//  Created by mobapp12 on 29/01/2019.
+//  Copyright © 2019 mobapp12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
